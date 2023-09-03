97a05a718c7f7baf554de53ff61a5c6561e2db69
melissasidisid <moimelly451@gmail.com>
2023-09-03 21:19:27 +0100

    test

---
+++ project-2.js
@@ -0,0 +1,10 @@
+alert ('welcome to this Madlibs')
+
+function show(){
+    let  name=document.getElementById('name').value;
+    let submit=document.getElementById('submit')
+    let Mobile=document.getElementById('Mobile').value;
+    document.write('your name is='+ name);
+   
+    document.write(' your Mobile is= '+ Mobile);  
+}
